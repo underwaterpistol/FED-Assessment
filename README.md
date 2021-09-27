6f8c6159b7acb9670ad90514306961fecf02c3b5
Danny <danny@underwaterpistol.com>
2021-09-27 10:07:43 +0100

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# UWP Frontend Technical Assessment
+
+This assessment is an opportunity for us to see how you approach a task and also how you execute it. In short, this task is to consume the response from a GraphQL request to the products endpoint, then generate and display at least 3 product cards from this information. Product cards should contain at a minimum:
+* An image
+* A title
+* A price
+* Product tags
+
+Since you have applied for a frontend position, we have supplied the GraphQL request for you and linked it to the fetch button already.
+
+We are evaluating:
+* The readability of your code
+* How you utilize git 
+* How maintainable/conventional your code is
+* How closely your submission follows the brief above
+* The responsiveness of your solution
+
+We are NOT looking for the fastest/shortest solution. We are not evaluating you on your design ability, however we do still want to see an actual product card. We recommend using VS Code for this assessment. The "Live Sass Compiler" extension will allow you to work in sass in realtime, and the Live Server extension will give you a realtime preview of your work.
+
+To get started you should fork off this repo. Once your work is complete, update the main branch on your fork and send it back to us.
---
+++ script.js
@@ -0,0 +1,86 @@
+const Products = {
+
+  /**
+   * Takes a JSON representation of the products and renders cards to the DOM
+   * @param {Object} productsJson 
+   */
+  displayProducts: productsJson => {
+
+    // Render the products here
+
+  },
+
+  state: {
+    storeUrl: "https://api-demo-store.myshopify.com/api/2020-07/graphql",
+    contentType: "application/json",
+    accept: "application/json",
+    accessToken: "b8385e410d5a37c05eead6c96e30ccb8"
+  },
+
+  /**
+   * Sets up the query string for the GraphQL request
+   * @returns {String} A GraphQL query string
+   */
+  query: () => `
+    {
+      products(first:3) {
+        edges {
+          node {
+            id
+            handle
+            title
+            tags
+            images(first:1) {
+              edges {
+                node {
+                  originalSrc
+                }
+              }
+            }
+            priceRange {
+              minVariantPrice {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+
+  /**
+   * Fetches the products via GraphQL then runs the display function
+   */
+  handleFetch: async () => {
+    const productsResponse = await fetch(Products.state.storeUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": Products.state.contentType,
+        "Accept": Products.state.accept,
+        "X-Shopify-Storefront-Access-Token": Products.state.accessToken
+      }, 
+      body: JSON.stringify({
+        query: Products.query()
+      })
+    });
+    const productsResponseJson = await productsResponse.json();
+    Products.displayProducts(productsResponseJson);
+  },
+
+  /**
+   * Sets up the click handler for the fetch button
+   */
+  initialize: () => {
+    // Add click handler to fetch button
+    const fetchButton = document.querySelector(".fetchButton");
+    if (fetchButton) {
+      fetchButton.addEventListener("click", Products.handleFetch);
+    }
+  }
+
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  Products.initialize();
+});
